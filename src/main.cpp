0219a624146b7477589f4f9e2845a7f0c7887d3b
less-hub <alessiocellucci.dev@gmail.com>
2022-11-06 02:14:25 +0100

    feat(global): add window

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,32 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 
+#include "RenderWindow.hpp"
+
 int main(int argc, char *args[])
 {
-    std::cout << "Yay, I'm Working!" << std::endl;
-    std::cin.get();
+    if (SDL_Init(SDL_INIT_VIDEO) > 0)
+        std::cout << "SDL_Init FAILED LOADING. SDL_ERROR: " << SDL_GetError() << std::endl;
+
+    if (!(IMG_Init(IMG_INIT_PNG)))
+        std::cout << "IMG_Init FAILED LOADING. SDL_ERROR: " << SDL_GetError() << std::endl;
+
+    RenderWindow window("GAME v1.0", 1280, 720);
+
+    bool gameRunning = true;
+
+    SDL_Event event;
+
+    while (gameRunning)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                gameRunning = false;
+        }
+    }
+
+    window.cleanUp();
+    SDL_Quit();
     return 0;
 }
